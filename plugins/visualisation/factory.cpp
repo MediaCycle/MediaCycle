33cedb33832a63e08db2be1da9a78919b1da403e
Damien Tardieu <damien.tardieu@fpms.ac.be>
2009-11-05 14:49:00 +0100

    error correction

--- plugins/visualisation/factory.cpp
+++ plugins/visualisation/factory.cpp
@@ -9,7 +9,6 @@
 
 extern "C" ACPlugin* create(std::string namePlugin) {
     if (namePlugin == "ACVisualisationPlugin") {return new ACVisualisationPlugin();}
-    if (namePlugin == "ACVisualisationPlugin2") {return new ACVisualisationPlugin();}
 }
 
 extern "C" void destroy(ACPlugin* d) {
@@ -19,7 +18,6 @@ extern "C" void destroy(ACPlugin* d) {
 extern "C" std::vector<std::string> list() {    //returns a string vector containing the plugin names included in the DLL file
     std::vector<std::string> listPlugin;
     listPlugin.push_back("ACVisualisationPlugin");
-    listPlugin.push_back("ACVisualisationPlugin2");
     //listPlugin.push_back("...");
     return listPlugin;
 }
