278253e5bfd1d3fd08d5a51952e1c199f7053a8b
Damien Tardieu <damien.tardieu@fpms.ac.be>
2010-06-08 11:40:04 +0200

    Ajout des fichier pour la prise en charge des plugin vamp

---
+++ plugins/vamp/ACVampDemoPlugin.cpp
@@ -0,0 +1,67 @@
+/*
+ *  ACVampDemoPlugin.cpp
+ *  MediaCycle
+ *
+ *  Created by Damien Tardieu
+ *  @copyright (c) 2010 – UMONS - Numediart
+ *  
+ *  MediaCycle of University of Mons – Numediart institute is 
+ *  licensed under the GNU AFFERO GENERAL PUBLIC LICENSE Version 3 
+ *  licence (the “License”); you may not use this file except in compliance 
+ *  with the License.
+ *  
+ *  This program is free software: you can redistribute it and/or 
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *  
+ *  Each use of this software must be attributed to University of Mons – 
+ *  Numediart Institute
+ *  
+ *  Any other additional authorizations may be asked to avre@umons.ac.be 
+ *  <mailto:avre@umons.ac.be>
+ *
+ */
+
+#include "ACVampDemoPlugin.h"
+#include "vamp-plugin-interface.h"
+#include "ACAudio.h"
+
+#include <vector>
+#include <string>
+
+//class ACMedia;
+
+ACVampDemoPlugin::ACVampDemoPlugin() {
+    //vars herited from ACPlugin
+    this->mMediaType = MEDIA_TYPE_AUDIO;
+    this->mPluginType = PLUGIN_TYPE_FEATURES;
+    this->mName = "Vamp";
+    this->mDescription = "Vamp plugin";
+    this->mId = "";
+}
+
+ACVampDemoPlugin::~ACVampDemoPlugin() {
+}
+
+
+std::vector<ACMediaFeatures*> ACVampDemoPlugin::calculate(ACMediaData* audio_data, ACMedia* theMedia) {
+	std::vector<ACMediaTimedFeatures*> descmf;
+	std::vector<ACMediaFeatures*> desc;
+	//	int sr = ((ACAudio*)theMedia)->getSampleRate();
+	ACAudio* theAudio = (ACAudio*) theMedia;
+  descmf.push_back(runPlugin("qm-vamp-plugins", "qm-chromagram", "0", 0, theAudio->getFileName(), 0));
+
+	for (int i=0; i<descmf.size(); i++)
+		desc.push_back(descmf[i]->mean());
+	return desc;
+}
+
---
+++ plugins/vamp/factory.cpp
@@ -0,0 +1,23 @@
+/*
+ *
+ */
+
+#include "ACPlugin.h"
+#include "ACVampDemoPlugin.h"
+
+//the factories
+
+extern "C" ACPlugin* create(std::string namePlugin) {
+	if (namePlugin == "ACVampDemoPlugin") {return new ACVampDemoPlugin();}
+}
+
+extern "C" void destroy(ACPlugin* d) {
+    delete d;
+}
+
+extern "C" std::vector<std::string> list() {    //returns a string vector containing the plugin names included in the DLL file
+    std::vector<std::string> listPlugin;
+    listPlugin.push_back("ACVampDemoPlugin");
+    //listPlugin.push_back("...");
+    return listPlugin;
+}
