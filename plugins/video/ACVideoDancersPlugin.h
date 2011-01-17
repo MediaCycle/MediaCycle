da725b5abbe162754b7548d633bebb09b5723859
Xavier Siebert <xavier.siebert@fpms.ac.be>
2011-01-17 15:15:22 +0100

    changed ACVideoPlugin to ACVideoDancersPlugin

---
+++ plugins/video/ACVideoDancersPlugin.h
@@ -0,0 +1,73 @@
+/*
+ *  ACVideoDancersPlugin.h
+ *  MediaCycle
+ *
+ *  @author Xavier Siebert
+ *  @date 22/09/09
+ *  @copyright (c) 2009 – UMONS - Numediart
+ *  
+ *  MediaCycle of University of Mons – Numediart institute is 
+ *  licensed under the GNU AFFERO GENERAL PUBLIC LICENSE Version 3 
+ *  licence (the “License”); you may not use this file except in compliance 
+ *  with the License.
+ *  
+ *  This program is free software: you can redistribute it and/or 
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *  
+ *  Each use of this software must be attributed to University of Mons – 
+ *  Numediart Institute
+ *  
+ *  Any other additional authorizations may be asked to avre@umons.ac.be 
+ *  <mailto:avre@umons.ac.be>
+ *
+ */
+
+// XS: this was named ACVideoPlugin before
+// on 17/01/2011 it was changed to ACVideoDancersPlugin 
+// because it is very specific to the Dancers! project
+
+#ifndef _ACVIDEODANCERSPLUGIN_H
+#define	_ACVIDEODANCERSPLUGIN_H
+
+#include "ACVideoAnalysis.h"
+#include "ACPlugin.h"
+#include "ACMediaFeatures.h"
+#include "ACMediaTimedFeature.h"
+
+#include<iostream>
+
+class ACVideoDancersPlugin : public ACPlugin {
+public:
+    ACVideoDancersPlugin();
+    ~ACVideoDancersPlugin();
+	std::vector<ACMediaFeatures*> calculate(std::string aFileName);
+	std::vector<ACMediaFeatures*> calculate(ACMediaData* _data, ACMedia*);
+
+	std::vector<ACMediaFeatures*>  calculateTop(std::string aFileName);
+	std::vector<ACMediaFeatures*>  calculateFront(std::string aFileName);
+	std::string changeLastFolder(std::string path, std::string folder);
+	std::string extractDirectory(std::string path);
+	std::string extractFilename(std::string path);
+	
+private:
+	ACMediaFeatures* calculateMeanOfTrajectory(ACVideoAnalysis* video);
+	ACMediaFeatures* calculateStdOfTrajectory(ACVideoAnalysis* video);
+	ACMediaFeatures* calculateMaxOfTrajectory(ACVideoAnalysis* video);
+	ACMediaFeatures* calculateContractionIndex(ACVideoAnalysis* video);
+	ACMediaFeatures* calculateMeanSpeedOfTrajectory(ACVideoAnalysis* video);
+//	ACMediaFeatures* calculateMostOccupiedCell(ACVideoAnalysis* video);
+	ACMediaFeatures* calculateMeanBoundingBoxRatio(ACVideoAnalysis* video);
+	ACMediaFeatures* calculateMeanPixelSpeed(ACVideoAnalysis* video);
+};
+
+#endif	/* _ACVIDEODANCERSPLUGIN_H */
