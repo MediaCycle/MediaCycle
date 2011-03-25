7286b7f4b6f25163aaf9e876f38be2eed3315af5
Thierry Ravet <thierry.ravet@umons.ac.be>
2011-03-25 10:56:10 +0100

    Add ACPlugin.cpp

---
+++ core/ACPlugin.cpp
@@ -0,0 +1,72 @@
+/*
+ *  ACPlugin.cpp
+ *  MediaCycle
+ *
+ *  @author Thierry Ravet
+ *  @date 24/03/11
+ *  @copyright (c) 2011 – UMONS - Numediart
+ *  
+ *  MediaCycle of University of Mons – Numediart institute is 
+ *  licensed under the GNU AFFERO GENERAL PUBLIC LICENSE Version 3 
+ *  licence (the “License”); you may not use this file except in compliance 
+ *  with the License.
+ *  
+ *  This program is free software: you can redistribute it and/or 
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *  
+ *  Each use of this software must be attributed to University of Mons – 
+ *  Numediart Institute
+ *  
+ *  Any other additional authorizations may be asked to avre@umons.ac.be 
+ *  <mailto:avre@umons.ac.be>
+ *
+ */
+
+
+
+#include "ACPlugin.h"
+
+
+ACPlugin::ACPlugin()
+{
+	cout<<"ACPlugin Constructor\n";
+	this->mPluginType=PLUGIN_TYPE_NONE;
+}
+
+bool ACPlugin::isPlugintype(ACPluginType pType)
+{
+	return (mPluginType&pType!=0);
+}
+
+ACFeaturesPlugin::ACFeaturesPlugin()
+{
+	cout<<"ACPlugin feature Constructor\n";
+	this->mPluginType=mPluginType|PLUGIN_TYPE_FEATURES;
+	
+}
+
+
+ACSegmentationPlugin::ACSegmentationPlugin()
+{
+	cout<<"ACPlugin segmentation Constructor\n";
+	this->mPluginType=mPluginType|PLUGIN_TYPE_SEGMENTATION;
+	
+}
+
+
+ACNeighborhoodsPlugin::ACNeighborhoodsPlugin() {
+	
+	this->mPluginType=mPluginType|PLUGIN_TYPE_NEIGHBORS_METHOD;
+	
+    //local vars
+}
