5edf518b1a20a37f5939604b82bcabb1ed3fbc29
Xavier Siebert <xavier.siebert@umons.ac.be>
2011-05-28 00:50:30 +0200

    - videoTest using OpenCV 2.0  -- check if it displays the correct number of frames (on Mac Leopard it works with ffmpeg)

--- tests/ugly/video/videoTest.cpp
+++ tests/ugly/video/videoTest.cpp
@@ -506,6 +506,22 @@ void test_Thomas(){
 	
 }
 
+void test_video_2_0(string s){
+	cv::VideoCapture cap; 
+	if (s=="") return;
+	cap.open(s);
+	cout << cap.get(CV_CAP_PROP_FRAME_COUNT) << " frames at " << cap.get(CV_CAP_PROP_FPS) << " frames/second"  << endl;
+	cv::Mat frame; 
+	cv::namedWindow("video", 1); 
+	for(;;) { 
+		cap >> frame; 
+		if(!frame.data) break; 
+		cout << cap.get(CV_CAP_PROP_POS_FRAMES) << " - " << cap.get(CV_CAP_PROP_POS_MSEC) << endl;
+		cv::imshow("video", frame); 
+		if(cv::waitKey(30) >= 0) break; 
+	}
+}
+
 int main(int argc, char** argv) {
 	cout << "Using Opencv " << CV_VERSION << "(" << CV_MAJOR_VERSION << "." << CV_MINOR_VERSION  << "." <<  CV_SUBMINOR_VERSION << ")" << endl;	
 
@@ -515,7 +531,7 @@ int main(int argc, char** argv) {
 	//test_histogram_equalize("Bru_105#2");
 	//test_bg_substraction("Bru_105#2");
 	// test_bg_substraction("Bru_203#2");
-	//test_browse("/usr/local/share/mediacycle/data/video/001011.mov");
+	test_video_2_0("/usr/local/share/mediacycle/data/video/001011.mov");
 	//test_browse("/Users/xavier/numediart/Project11.1-MediaBlender/test/video/elephantsdream-480-h264-st-aac.mov");
 	//test_read_write_video(videodir+"Front/001011.mov");
 //	test_optical_flow(videodir+"Front/001011.mov");
@@ -526,7 +542,7 @@ int main(int argc, char** argv) {
 
 	// christian graupner videos
 	//test_blobs("10151");
-	test_video_pixel_speed_plugin("/Users/xavier/numediart/Project11.1-MediaBlender/test/video/elephantsdream-480-h264-st-aac.mov"); //
+//	test_video_pixel_speed_plugin("/Users/xavier/numediart/Project11.1-MediaBlender/test/video/elephantsdream-480-h264-st-aac.mov"); //
 	///Users/xavier/numediart/Project10.1-Borderlands/2010_4_prox_alpa/10151.mov");
 	// 10603
 //	test_video_hu_moments("/Users/xavier/numediart/Project10.1-Borderlands/2010_4rgb_alpha/10151.mov",
