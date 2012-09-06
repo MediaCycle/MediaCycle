6ef3f305c09d8c96c158a6c51ad62e158418cf55
Christian Frisson <christian.frisson@gmail.com>
2012-09-06 11:54:38 +0200

    Updated credits of the PDF features plugin

--- plugins/pdf/ACPDFFeaturesPlugin.cpp
+++ plugins/pdf/ACPDFFeaturesPlugin.cpp
@@ -1,33 +1,35 @@
-/**
- * @brief ACPDFFeaturesPlugin.cpp
- * @author Xavier Siebert
- * @date 18/07/2011
- * @copyright (c) 2011 – UMONS - Numediart
- * 
- * MediaCycle of University of Mons – Numediart institute is 
- * licensed under the GNU AFFERO GENERAL PUBLIC LICENSE Version 3 
- * licence (the “License”); you may not use this file except in compliance 
- * with the License.
- * 
- * This program is free software: you can redistribute it and/or 
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- * 
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- * 
- * Each use of this software must be attributed to University of Mons – 
- * Numediart Institute
- * 
- * Any other additional authorizations may be asked to avre@umons.ac.be 
- * <mailto:avre@umons.ac.be>
-*/
+/*
+ *  ACPDFFeaturesPlugin.cpp
+ *  MediaCycle
+ *
+ *  @author Christian Frisson
+ *  @date 28/05/2011
+ *  @copyright (c) 2011 – UMONS - Numediart
+ *  
+ *  MediaCycle of University of Mons – Numediart institute is 
+ *  licensed under the GNU AFFERO GENERAL PUBLIC LICENSE Version 3 
+ *  licence (the “License”); you may not use this file except in compliance 
+ *  with the License.
+ *  
+ *  This program is free software: you can redistribute it and/or 
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *  
+ *  Each use of this software must be attributed to University of Mons – 
+ *  Numediart Institute
+ *  
+ *  Any other additional authorizations may be asked to avre@umons.ac.be 
+ *  <mailto:avre@umons.ac.be>
+ */
 
 #include "ACPDFFeaturesPlugin.h"
 #include "ACPDF.h"
