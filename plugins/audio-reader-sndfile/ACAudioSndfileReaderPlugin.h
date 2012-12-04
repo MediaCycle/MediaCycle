1da14e01ba852fa95de16388893b82a198786620
Christian Frisson <christian.frisson@gmail.com>
2012-12-04 01:08:04 +0100

    Convenience ACAudioSndfileReaderPlugin

---
+++ plugins/audio-reader-sndfile/ACAudioSndfileReaderPlugin.h
@@ -0,0 +1,49 @@
+/*
+ *  ACAudioSndfileReaderPlugin.h
+ *  MediaCycle
+ *
+ *  @author Christian Frisson
+ *  @date 14/10/12
+ *  @copyright (c) 2012 – UMONS - Numediart
+ *  
+ *  MediaCycle of University of Mons – Numediart institute is 
+ *  licensed under the GNU AFFERO GENERAL PUBLIC LICENSE Version 3 
+ *  licence (the “License”); you may not use this file except in compliance 
+ *  with the License.
+ *  
+ *  This program is free software: you can redistribute it and/or 
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *  
+ *  Each use of this software must be attributed to University of Mons – 
+ *  Numediart Institute
+ *  
+ *  Any other additional authorizations may be asked to avre@umons.ac.be 
+ *  <mailto:avre@umons.ac.be>
+ *
+ */
+
+#include <ACPlugin.h>
+
+#ifndef ACAudioSndfileReaderPlugin_H
+#define	ACAudioSndfileReaderPlugin_H
+
+class ACAudioSndfileReaderPlugin : public ACMediaReaderPlugin
+{
+public:
+    ACAudioSndfileReaderPlugin();
+    ~ACAudioSndfileReaderPlugin();
+    virtual ACMedia* mediaFactory(ACMediaType mediaType, const ACMedia* media = 0);
+    virtual std::map<std::string, ACMediaType> getSupportedExtensions(ACMediaType media_type = MEDIA_TYPE_ALL);
+};
+
+#endif
