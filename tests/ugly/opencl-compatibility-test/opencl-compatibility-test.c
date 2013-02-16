73c6e3bc9a27f80d3c5153c1c5604724ba02df2d
Christian Frisson <christian.frisson@gmail.com>
2013-02-16 17:39:41 +0100

    Added an executable test to check the compatibility of the machine with OpenCL

---
+++ tests/ugly/opencl-compatibility-test/opencl-compatibility-test.c
@@ -0,0 +1,53 @@
+/**
+ * @brief opencl-compatibility-test.c
+ * @author Christian Frisson
+ * @date 16/02/2013
+ * @copyright (c) 2013 – UMONS - Numediart
+ * 
+ * MediaCycle of University of Mons – Numediart institute is 
+ * licensed under the GNU AFFERO GENERAL PUBLIC LICENSE Version 3 
+ * licence (the “License”); you may not use this file except in compliance 
+ * with the License.
+ * 
+ * This program is free software: you can redistribute it and/or 
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * 
+ * Each use of this software must be attributed to University of Mons – 
+ * Numediart Institute
+ * 
+ * Any other additional authorizations may be asked to avre@umons.ac.be 
+ * <mailto:avre@umons.ac.be>
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <OpenCL/opencl.h>
+
+int main(int argc, char* const argv[]) {
+    cl_uint num_devices, i;
+    clGetDeviceIDs(NULL, CL_DEVICE_TYPE_ALL, 0, NULL, &num_devices);
+
+    cl_device_id* devices = calloc(sizeof(cl_device_id), num_devices);
+    clGetDeviceIDs(NULL, CL_DEVICE_TYPE_ALL, num_devices, devices, NULL);
+
+    char buf[128];
+    for (i = 0; i < num_devices; i++) {
+        clGetDeviceInfo(devices[i], CL_DEVICE_NAME, 128, buf, NULL);
+        fprintf(stdout, "Device %s supports ", buf);
+
+        clGetDeviceInfo(devices[i], CL_DEVICE_VERSION, 128, buf, NULL);
+        fprintf(stdout, "%s\n", buf);
+    }
+
+    free(devices);
+}
