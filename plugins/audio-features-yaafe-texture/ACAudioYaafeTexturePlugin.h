5f735b8e6e14f5d8cd312e36de97de9e0f92a0b1
Thierry Ravet <thierry.ravet@umons.ac.be>
2013-05-16 16:51:45 +0200

    audio texture feature extraction configuration

---
+++ plugins/audio-features-yaafe-texture/ACAudioYaafeTexturePlugin.h
@@ -0,0 +1,45 @@
+/**
+ * @brief MediaCycle plugin to extract Texture Yaafe features
+ * @author Christian Frisson
+ * @date 31/03/2012
+ * @copyright (c) 2012 – UMONS - Numediart
+ * 
+ * MediaCycle of University of Mons – Numediart institute is 
+ * licensed under the GNU AFFERO GENERAL PUBLIC LICENSE Version 3 
+ * licence (the “License”); you may not use this file except in compliance 
+ * with the License.
+ * 
+ * This program is free software: you can redistribute it and/or 
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * 
+ * Each use of this software must be attributed to University of Mons – 
+ * Numediart Institute
+ * 
+ * Any other additional authorizations may be asked to avre@umons.ac.be 
+ * <mailto:avre@umons.ac.be>
+ */
+
+#ifndef ACAudioYaafeTexturePlugin_H
+#define	ACAudioYaafeTexturePlugin_H
+
+#include "ACAudioYaafeCorePlugin.h"
+
+class ACAudioYaafeTexturePlugin : public ACAudioYaafeCorePlugin {
+
+public:
+    ACAudioYaafeTexturePlugin();
+    virtual ~ACAudioYaafeTexturePlugin(){}
+    virtual std::string dataflowFilename(){return "ACAudioYaafeTexturePluginSettings.txt";}
+};
+
+#endif	/* ACAudioYaafeTexturePlugin_H */
