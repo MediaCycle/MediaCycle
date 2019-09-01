c26e174c517af811d489bd269fb76ddc70e2c038
Christian Frisson <christian.frisson@gmail.com>
2019-09-01 12:21:43 -0400

    armadillo-utils: fix long long signatures

--- libs/armadillo-utils/findpeaks.h
+++ libs/armadillo-utils/findpeaks.h
@@ -1,8 +1,8 @@
 /**
  * @brief findpeaks.h
  * @author Christian Frisson
- * @date 11/04/2013
- * @copyright (c) 2013 – UMONS - Numediart
+ * @date 01/09/2019
+ * @copyright (c) 2019 – UMONS - Numediart
  * 
  * MediaCycle of University of Mons – Numediart institute is 
  * licensed under the GNU AFFERO GENERAL PUBLIC LICENSE Version 3 
@@ -35,12 +35,17 @@
 
 #include "Armadillo-utils.h"
 
-template<typename eT>
-arma::icolvec findpeaks(const arma::Col<eT>& Data, long Pd){
-	long m = 0;                  // counter for peaks found
-	long L = Data.n_rows;
+#include <algorithm>
+#include <iostream>
+#include <string>
 
-	long j = 0;
+template<typename eT>
+arma::icolvec findpeaks(const arma::Col<eT>& Data, long Pd)
+{
+	long m = 0;                  // counter for peaks found
+	long long L = Data.n_rows;
+
+	long long j = 0;
 	arma::Col<eT> pks  = arma:: zeros< arma::Col<eT> >(Data.n_rows);
 	arma::icolvec locs = arma:: zeros<arma::icolvec>(Data.n_rows);
 
@@ -50,34 +55,34 @@ arma::icolvec findpeaks(const arma::Col<eT>& Data, long Pd){
 	long endL, endR;
 	while (j < L){
 		// leftmost neighbor index
-		endL = std::max((long) 0,j-Pd);  
-    
-    // Update the leftmost neighbor index if there is a peak within
-    // "Pd" neighbors of leftmost index
-    if(m >= 0){
+		endL = std::max((long long) 0,j-Pd);
+
+		// Update the leftmost neighbor index if there is a peak within
+		// "Pd" neighbors of leftmost index
+		if(m >= 0){
 			if (j < std::min(locs(m)+Pd, L-1)){
 				j = std::min(locs(m)+Pd, L-1);
 				endL = j-Pd;
 			}
 		}
-   
-    // rightmost neighbor index
-    endR = std::min(L-1,j+ Pd);   
-    
-    // create neighbor data set
-    arma::Col<eT> temp = Data.rows(endL,endR); 
-   
-    // set current data point to -Inf in the neighbor data set
-    temp(j-endL) = min(temp)-1;
+
+		// rightmost neighbor index
+		endR = std::min(L-1,j+ Pd);
+
+		// create neighbor data set
+		arma::Col<eT> temp = Data.rows(endL,endR);
+
+		// set current data point to -Inf in the neighbor data set
+		temp(j-endL) = min(temp)-1;
 		
-        // Compare current data point with all neighbors-    
+		// Compare current data point with all neighbors-
 #ifdef __clang__
-        if( ((sum(Data(j) > temp)) == temp.n_rows) && (j != 0) && j!=L-1 ){
+		if( ((sum(Data(j) > temp)) == temp.n_rows) && (j != 0) && j!=L-1 ){
 #else //__clang__
-            if( (arma::as_scalar(sum(Data(j) > temp)) == temp.n_rows) && (j != 0) && j!=L-1 ){
+		if( (arma::as_scalar(sum(Data(j) > temp)) == temp.n_rows) && (j != 0) && j!=L-1 ){
 #endif //__clang__
-			pks(m)  = Data(j); 
-			locs(m) = j;       
+			pks(m)  = Data(j);
+			locs(m) = j;
 			m = m+1;
 		}
 		j =j+1;
@@ -89,8 +94,8 @@ arma::icolvec findpeaks(const arma::Col<eT>& Data, long Pd){
 		locs = locs.rows(0,m-1);
 	}
 	else{
-    pks.reset();
-    locs.reset();
+		pks.reset();
+		locs.reset();
 	}
 	return locs;
 }
